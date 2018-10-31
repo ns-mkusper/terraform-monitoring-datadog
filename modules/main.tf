bbf3c59f24d29bfa0249b0f5c632daf00222f883
Mark Kusper <mark.kusper@ge.com>
2018-10-30 20:11:23 -0700

    Change VPC Name.

--- modules/main.tf
+++ modules/main.tf
@@ -6,9 +6,10 @@
 
 data "aws_caller_identity" "current" {}
 
+# TODO: Add to DevOps VPC instead of creating a new one.
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  name = "vpc-example"
+  name = "vpc-monitoring"
   cidr = "10.0.0.0/16"
   azs             = ["${data.aws_availability_zones.available.names}"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
